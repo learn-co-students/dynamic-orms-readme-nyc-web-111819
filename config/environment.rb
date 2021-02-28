d74d4622891d16b2deb59a2508a35ef8f292c292
Miss-Cheese <nat.galligan@gmail.com>
2021-02-28 17:16:28 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
 require 'sqlite3'
 
 
-DB = {:conn => SQLite3::Database.new("db/songs.db")}
-DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB = {:conn => SQLite3::Database.new("db/books.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS books")
 
 sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS songs (
+  CREATE TABLE IF NOT EXISTS books (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  album TEXT
+  author TEXT
   )
 SQL
 
